29e7edaa71d2cb1ebac4b243879cb366a09aa094
delba <delba@users.noreply.github.com>
2015-03-21 17:47:34 -0400

    Add content and title subviews

--- OEmbed-client/ItemViewController.swift
+++ OEmbed-client/ItemViewController.swift
@@ -12,7 +12,26 @@ class ItemViewController: UIViewController {
     
     // MARK: - Data
     
-    var item: Item!
+    var item: Item! {
+        didSet {
+            render()
+        }
+    }
+    
+    // MARK: - Subviews
+    
+    lazy var content: UIView = {
+        let view = UIView()
+        view.setTranslatesAutoresizingMaskIntoConstraints(false)
+        view.backgroundColor = UIColor.blackColor()
+        return view
+    }()
+    
+    lazy var itemTitle: UILabel = {
+        let label = UILabel()
+        label.setTranslatesAutoresizingMaskIntoConstraints(false)
+        return label
+    }()
     
     // MARK: - UIViewController
     
@@ -21,7 +40,36 @@ class ItemViewController: UIViewController {
         
         title = item.title
         
+        edgesForExtendedLayout = .None
+        
         view.backgroundColor = UIColor.whiteColor()
+        
+        view.addSubview(content)
+        view.addSubview(itemTitle)
+        
+        updateViewConstraints()
+    }
+    
+    // MARK: - Constraints
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        let views = [
+            "content": content,
+            "title": itemTitle
+        ]
+        
+        view.addConstraint(NSLayoutConstraint(item: content, attribute: .Height, relatedBy: .Equal, toItem: content, attribute: .Width, multiplier: 2 / 3, constant: 0))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[content]|", options: nil, metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[title]|", options: nil, metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[content]-[title]", options: nil, metrics: nil, views: views))
+    }
+    
+    // MARK: - Render
+    
+    func render() {
+        itemTitle.text = item.title
     }
 
 }
